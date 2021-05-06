1334114e8f7ee2f8e6c368e643c59b036ea7c4b4
Md Touseef <mdtouseef15@gmail.com>
2021-05-07 03:04:17 +0530

    working hard

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,8 @@
+console.log("hi");
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
 
-         const menuToggle = document.querySelector('.toggle');
-      const showcase = document.querySelector('.showcase');
-
-      menuToggle.addEventListener('click', () => {
+menuToggle.addEventListener('click', ()=>{
         menuToggle.classList.toggle('active');
         showcase.classList.toggle('active');
-      })
-    
+    })
